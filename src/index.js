2cca32118c288f9ed12727b112c86287c5c65704
4-o-4 <yuzhakov.lev@gmail.com>
2020-10-25 20:50:00 +0400

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = [],
+        i = 0;
+
+    while (bracketsConfig.length > i) {
+        brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+        i++;
+    }
+
+    let newStr = str;
+    while (true) {
+        i = 0;
+        while (brackets.length > i) {
+            str = str.replace(brackets[i], "");
+            i++;
+        }
+
+        if (str.length == 0) {
+            return true;
+        }
+        if (newStr === str) {
+            return false;
+        }
+
+        newStr = str;
+    }
+};
